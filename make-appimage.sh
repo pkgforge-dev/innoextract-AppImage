f99c022a96bf81844eb4b8b2f450ffd5b4be32c4
Link4Electronics <link4electronics@gmail.com>
2026-08-15 07:00:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q innoextract | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q innoextract | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -13,8 +13,6 @@ export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/innoextract
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
