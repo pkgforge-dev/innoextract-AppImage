6dccfa055e62d3ffd9fc0809f7c7c0c6a5e1f629
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:01:17 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,7 +16,3 @@ quick-sharun /usr/bin/innoextract
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --simple-test ./dist/*.AppImage
